059c4991e1739d3e2e4dd4f691203177d2e743b2
Peter Liu <spusdpeter@gmail.com>
2019-10-29 12:36:48 -0700

    Finished

--- app/src/main/java/com/example/quiz/ScoreActivity.java
+++ app/src/main/java/com/example/quiz/ScoreActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity
 {
     private TextView scoreText;
+    private Button restart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,11 +28,29 @@ public class ScoreActivity extends AppCompatActivity
 
         //
         scoreText.setText(score + "/10");
+        setListeners();
     }
+
+    public void setListeners()
+    {
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent targetIntent = new Intent(ScoreActivity.this, MainActivity.class);
+                startActivity(targetIntent);
+
+            }
+        });
+
+    }
+
 
     public void wireWidgets()
     {
         scoreText = findViewById(R.id.textView_score_score);
+        restart = findViewById(R.id.button_restart_score);
+
 
     }
 }
